a4987b1297064a2c026d4e9014769016fd30d8b9
yuvalGa <142139177+yuvalGa@users.noreply.github.com>
2025-05-05 15:18:31 +0300

    Update storageAccount.tf

--- storageAccount.tf
+++ storageAccount.tf
@@ -4,7 +4,6 @@ resource "azurerm_storage_account" "example" {
   location                 = var.location
   account_tier             = "Standard"
   account_kind             = "StorageV2"
-  account_replication_type = "GRS"
 }
 
 variable "storage" {
