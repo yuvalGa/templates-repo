866f7ae10754593d47e1c7262b336b16c85690e9
yuvalGa <142139177+yuvalGa@users.noreply.github.com>
2025-05-05 15:22:01 +0300

    Create main.tf

---
+++ ephemeral/storage-account/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage
+  resource_group_name      = var.resource_group
+  location                 = var.location
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  account_replication_type = "GRS"
+}
+
+variable "storage" {
+  description = "The name of the Storage Account to create."
+  type        = string
+  default     = null
+}
+
+variable "resource_group" {
+  description = "Name of the existing Azure Resource Group in which the Storage Account will be created."
+  type        = string
+  default     = null
+}
+
+variable "location" {
+  description = "Azure region where the Storage Account and associated resources will be deployed (e.g., eastus, westeurope)."
+  type        = string
+  default     = null
+}
